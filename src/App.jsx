b15d1f32ffcf6b1a5409576c02df0cda90838c8b
MD-ANIKS <freelanceranik1879@gmail.com>
2024-04-21 17:07:12 +0600

    Made the password generator mobile-friendly!

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,7 @@ function App() {
 
   return (
     <>
-      <div className="px-6 lg:px-32 text-neutral-200 w-full text-center flex justify-center items-center flex-col gap-3 lg:gap-8">
+      <div className="px-3 sm:px-6 lg:px-32 text-neutral-200 w-full text-center flex justify-center items-center flex-col gap-3 lg:gap-8">
         <h1 className="text-2xl lg:text-5xl font-bold capitalize mt-8">
           Random password generator
         </h1>
@@ -66,13 +66,13 @@ function App() {
         </p>
       </div>
 
-      <div className="mt-20 mb-6 lg:mt-32 px-6 md:px-28 lg:px-32 flex flex-col lg:flex-row justify-between items-center gap-20 lg:gap-40 text-neutral-200">
+      <div className="mt-20 mb-6 lg:mt-32 px-3 sm:px-6 md:px-28 lg:px-32 flex flex-col lg:flex-row justify-between items-center gap-20 lg:gap-32 xl:gap-40 text-neutral-200">
         <div className="w-[250px] lg:w-[40%]">
           <img className="w-full" src={safe} alt="" />
         </div>
 
         <div className="w-full lg:w-[70%] flex flex-col gap-y-6 lg:gap-y-12">
-          <div className="w-full flex gap-2 lg:gap-4 relative justify-between items-center">
+          <div className="w-full flex gap-2 lg:gap-4 relative sm:justify-between items-center">
             <input
               type="text"
               name="password"
@@ -83,14 +83,14 @@ function App() {
             />
             <FcRefresh onClick={passwordGenerator} className="absolute right-0 -translate-x-[80px] md:-translate-x-[90px] lg:-translate-x-[120px] z-50 bg-neutral-200 px-1 text-2xl lg:text-3xl cursor-pointer" />
 
-            <button className="bg-blue-600 text-neutral-200 font-bold uppercase text-xs lg:text-sm rounded-full w-20 lg:w-28 px-3 lg:px-5 py-2 lg:py-3 shadow-2xl shadow-blue-600" onClick={handleOnCopy}>
+            <button className="bg-blue-600 text-neutral-200 font-bold uppercase text-xs lg:text-sm rounded-full max-w-16 w-full lg:max-w-none lg:w-28 px-3 lg:px-5 py-2 lg:py-3 shadow-2xl shadow-blue-600" onClick={handleOnCopy}>
               {cText}
             </button>
           </div>
 
-          <div className="flex flex-col text-sm lg:text-base items-center justify-center gap-4">
-            <div className="flex w-full justify-between items-center gap-2 lg:gap-5">
-            <label className="w-full">
+          <div className="flex flex-col text-xs sm:text-sm lg:text-base items-center justify-center gap-4">
+            <div className="flex w-full flex-wrap sm:flex-nowrap justify-between items-center gap-2 lg:gap-5">
+            <label className="w-fit sm:w-full">
                 Password Length : <span className="font-bold">{length}</span>
               </label>
               <input
@@ -100,14 +100,14 @@ function App() {
                 onChange={(e) => setLength(e.target.value)}
                 min={8}
                 max={60}
-                className="w-full"
+                className="w-2/4 sm:w-full"
               />
             </div>
 
-            <div className="flex flex-wrap justify-between items-center w-full gap-2 lg:gap-4">
+            <div className="flex flex-wrap sm:flex-nowrap justify-between items-center w-full gap-1 sm:gap-2 lg:gap-4">
             <label>Charecter Used :</label>
 
-            <div className="flex space-x-2 lg:space-x-8 tracking-wider">
+            <div className="flex space-x-2 lg:space-x-4 xl:space-x-8 tracking-wider">
               <div className="flex gap-1 lg:gap-3">
                 <input defaultChecked={uppercaseAllowed} onChange={() => setUppercaseAllowed((prev) => !prev)} type="checkbox" />
                 <label>ABC</label>
